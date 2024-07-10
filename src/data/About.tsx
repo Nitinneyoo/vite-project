b295d4494fa0969500bd22bd98248f008e17a6a9
NeYoO <nitinsinghji12@gmail.com>
2024-07-11 04:24:26 +0530

    Formate change

--- src/data/About.tsx
+++ src/data/About.tsx
@@ -1,12 +1,14 @@
-import { BentoGridDemo } from "@/components/Bento_grid"
+import { BentoGridDemo } from "@/components/Bento_grid";
 
 export const About = () => {
-    return (
-        <div>
-            <div>
-                <h1 className="text-5xl font-bold text-center text-white mb-10">Information about the school</h1>
-            </div>
-            <BentoGridDemo />
-        </div>
-    )
-}
+  return (
+    <div>
+      <div>
+        <h1 className="text-5xl font-bold text-center text-white mb-10">
+          Information about the school
+        </h1>
+      </div>
+      <BentoGridDemo />
+    </div>
+  );
+};
